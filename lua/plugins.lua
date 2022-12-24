752cfe7a4364810ab7de93bb001cec5956f3ca2d
Nick <nick@moontower.net>
2022-12-24 10:59:30 -0600

    NerdTree

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -72,18 +72,10 @@ function M.setup()
     -- FZF
     use { "junegunn/fzf.vim" }
     -- Tree
-    -- Better Netrw
-    -- use {"tpope/vim-vinegar"}
-    -- use {
-    --  "kyazdani42/nvim-tree.lua",
-    --  requires = {
-    --    "kyazdani42/nvim-web-devicons",
-    --  },
-    --  cmd = { "NvimTreeToggle", "NvimTreeClose" },
-    --    config = function()
-    --      require("config.nvimtree").setup()
-    --    end,
-    -- }
+    use { "preservim/nerdtree" } 
+
+
+    -- Packer stuff
     if packer_bootstrap then
       print "Restart Neovim required after installation!"
       require("packer").sync()
