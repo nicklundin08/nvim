b97fabffde495ad482d9c9419cc53b3a3d977e22
Nick Lundin <nick@moontower.net>
2023-10-21 14:17:41 +0000

    Messing with config maps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = function(mode, lhs, rhs, desc)
+  vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc })
+end
+
+map("i", "jk", "<esc>")
+
+map("n", "gx", ":!echo hi<enter>")
+map("n", ";;", ":!")
