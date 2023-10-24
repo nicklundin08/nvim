9091a384369833ebbac380793ea0391ba1da75ca
Nick <nick@moontower.net>
2023-10-24 22:55:11 +0000

    Messing with toggle term

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,7 +6,25 @@ local map = function(mode, lhs, rhs, desc)
   vim.keymap.set(mode, lhs, rhs, { silent = true, desc = desc })
 end
 
-map("i", "jk", "<esc>")
-
 map("n", "gx", ":!echo hi<enter>")
-map("n", ";;", ":!")
+-- map("n", ";;", ":!")
+-- map("n", ";;", ":TermExec cmd='dotnet test'<enter>")
+-- map("n", "<S-\\>", ":TermExec cmd='echo hi'<enter>")
+
+require("toggleterm").setup {
+  open_mapping = [[<c-\>]],
+}
+
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
---
+++ lua/plugins/real.lua
@@ -0,0 +1,5 @@
+-- since this is just an example spec, don't actually load anything here and return an empty spec
+-- stylua: ignore
+return {
+  { 'akinsho/toggleterm.nvim', version = "*", opts = { --[[ things you want to change go here]] } }
+}
