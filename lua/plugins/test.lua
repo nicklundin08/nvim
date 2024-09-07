755f2ad60e4ffe5ec7427e8cb579ff162d91b442
Nick Lundin <nick@moontower.net>
2024-09-07 12:07:48 -0500

    Typescript working after rolling back to stable nvim version

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,44 +1,30 @@
 return {
-  {
-    "nvim-neotest/neotest",
-    dependencies = {
-      "haydenmeade/neotest-jest",
-      "marilari88/neotest-vitest",
-    },
-    keys = {
-      {
-        "<leader>tl",
-        function()
-          require("neotest").run.run_last()
-        end,
-        desc = "Run Last Test",
-      },
-      {
-        "<leader>tL",
-        function()
-          require("neotest").run.run_last({ strategy = "dap" })
-        end,
-        desc = "Debug Last Test",
-      },
-      {
-        "<leader>tw",
-        "<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>",
-        desc = "Run Watch",
-      },
-    },
-    opts = function(_, opts)
-      table.insert(
-        opts.adapters,
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "haydenmeade/neotest-jest",
+  },
+  config = function()
+    require("neotest").setup({
+      adapters = {
         require("neotest-jest")({
-          jestCommand = "npm test --",
-          jestConfigFile = "custom.jest.config.ts",
+          jestCommand = "npx jest --verbose --",
           env = { CI = true },
-          cwd = function()
+          cwd = function(path)
             return vim.fn.getcwd()
           end,
-        })
-      )
-      table.insert(opts.adapters, require("neotest-vitest"))
-    end,
-  },
+        }),
+      },
+    })
+
+    vim.keymap.set("n", "<leader>jf", function()
+      require("neotest").run.run(vim.fn.expand("%"))
+    end, { noremap = true, silent = true, desc = "Run all tests" })
+    vim.keymap.set("n", "<leader>jo", function()
+      require("neotest").output.open({ enter = true })
+    end, { noremap = true, silent = true, desc = "Open test output" })
+  end,
 }
